7bb3a8063e4f537c0d9c90f2ccd27a8fededdb83
ocampo13 <cristian.ocampob@campusucc.edu.co>
2026-09-10 02:06:25 +0000

    Commit 2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
-import json 
-
-with open("data/clientes.json", "r") as archivo:
-    clientes = json.load(archivo)
-
-for cliente in clientes:
-    print(clientes["nombre"])    
+import json
+ 
+from fastapi import FastAPI
+ 
+app = FastAPI()
+ 
+@app.get("/")
+def inicio():
+    return {
+        "mensaje": "Generación de la API"
+    }
+ 
+@app.get("/clientes")
+def obtener_clientes():
+    with open("data/clientes.json", "r") as archivo:
+        clientes = json.load(archivo)
+ 
+    return clientes
+ 
+@app.get("/clientes/{id_cliente}")
+def obtener_cliente(id_cliente: int):
+    with open("data/clientes.json", "r") as archivo:
+        clientes = json.load(archivo)
+ 
+    for cliente in clientes:
+        if cliente["id"] == id_cliente:
+            return cliente
+ 
+    return {
+        "error": "Cliente no encontrado"
+    }
